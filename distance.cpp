9496045d949ee267cac1ce47bac75f9f1eedfdcc
dgerard42 <daniella4422@gmail.com>
2019-08-29 12:18:09 -0700

    getting started. kinda bored.

---
+++ distance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+        int x1 = 2;
+        int x2 = 8;
+        int y1 = 3;
+        int y2 = -5;
+        
+}
