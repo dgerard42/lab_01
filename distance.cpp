0bca2e3b2bd09eaadd95717112531f3bb8c220da
dgerard42 <daniella4422@gmail.com>
2019-09-05 12:17:01 -0700

    i should just sit down and get this thing done early, so I can help people in class

--- distance.cpp
+++ distance.cpp
@@ -1,13 +1,24 @@
+#include <iostream>
 #include <string>
 #include <cmath>
+#include <limits.h>
 
 using namespace std;
 
 int main()
 {
-        int x1 = 2;
-        int x2 = 8;
-        int y1 = 3;
-        int y2 = -5;
-        
+    int distance;
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    cout << "this is a **distance calculator** for 2 points" << endl;
+    cout << "enter the coordinates of point 1, x then y:" << endl;
+    cin >> x1 >> y1;
+    cout << "enter the coordinates of point 2, x then y:" << endl;
+    cin >> x2 >> y2; 
+    distance = sqrt((pow((x1 - x2), 2)) + pow((y1 - y2), 2));
+    cout << "the distance between those 2 points is " << distance << endl;
+    return (0);
 }
