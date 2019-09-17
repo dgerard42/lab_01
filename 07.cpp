8e20608e785369483a95a5c5e95e724f8f2278ab
dgerard42 <daniella4422@gmail.com>
2019-09-17 12:20:45 -0700

    changed filenames. added grade calc and started dumb mmultiplication

---
+++ 07.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void    print_message(char grade)
+{
+    switch (grade)
+    {
+        case 'A':
+            cout << "Excellent" << endl;
+            break;
+        case 'B':
+            cout << "Very Good" << endl;
+            break;
+        case 'C':
+            cout << "Good" << endl;
+            break;
+        case 'D':
+        case 'F':
+            cout << "Try Harder Next Time" << endl;
+            break;
+        default:
+            cout << "Error. Invalid grade" << endl;
+    }
+}
+
+char    determine_grade(int score)
+{
+    if (0 <= score && score < 60)
+        return 'F';
+    else if (60 <= score && score < 70)
+        return 'D';
+    else if (70 <= score && score < 80)
+        return 'C';
+    else if (80 <= score && score < 90)
+        return 'B';
+    else if (90 <= score)
+        return 'A';
+    return 'E';
+}
+
+int     get_score()
+{
+    int     score;
+    bool    error = 1;
+
+    cout << "enter a number grade between 0 & 100:" << endl;
+    while (error == 1)
+    {
+        cin >> score;
+        if (score >= 1 && score <= 100)
+            error = 0;
+        else
+        {
+            cout << "you entered invalid input. try again." << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+    return score;
+}
+
+int     main()
+{
+    char    grade;
+    int     score;
+
+    score = get_score();
+    cout << "DEBUG 00 score=" << score << endl;
+    grade = determine_grade(score);
+    cout << "DEBUG 01 grade=" << grade << endl;
+    print_message(grade);
+    return 0;
+}
---
+++ 08.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int         dumb_multiplication(int num_1, int num_2)
+{
+    int result = 0;
+
+    while (num_1 > 0)
+    {
+        result += num_2;
+        num_1--;
+    }
+    return result;
+}
+
+
+int         main()
+{
+    int num_1;
+    int num_2;
+
+    cout << "welcome to >>>THE MULTIPLIER!!!<<< ╚═| ~ ಠ ₒ ಠ ~ |═╝" << endl;
+    
+
+}
